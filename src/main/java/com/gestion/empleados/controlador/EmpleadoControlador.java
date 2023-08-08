ce435c62377f7bf04b0b8b809f1652a0e3cc2ae4
SAMANTHITO <alfons17ver@gmail.com>
2023-08-08 09:04:37 -0500

    guarda los datos de agregar empleados

--- src/main/java/com/gestion/empleados/controlador/EmpleadoControlador.java
+++ src/main/java/com/gestion/empleados/controlador/EmpleadoControlador.java
@@ -1,22 +1,52 @@
 package com.gestion.empleados.controlador;
 
+import com.gestion.empleados.excepciones.ResourceNotFoundException;
 import com.gestion.empleados.modelo.Empleado;
 import com.gestion.empleados.repositorio.EmpleadoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class EmpleadoControlador {
 
     @Autowired
     private EmpleadoRepositorio repositorio;
+    // este metodo sirve para listar todos los empleados
     @GetMapping("/empleados")
     public List<Empleado> listarTodosLosEmpleados(){
         return repositorio.findAll();
     }
+
+    // este metodo sirve para guardar el empleado
+    @PostMapping("/empleados")
+    public Empleado guardarEmpleado(@RequestBody Empleado empleado){
+        return  repositorio.save(empleado);
+    }
+
+    //este metodo sirve para buscar un empleado
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> obtenerEmpleadoPorId(@PathVariable Long id){
+        Empleado empleado = repositorio.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException(("No existe el usuario con el ID: "+id)));
+        return ResponseEntity.ok(empleado);
+    }
+
+    /*@PutMapping("/empleado/{id}")
+    public ResponseEntity<Empleado> actualizarEmpleado(@PathVariable Long id,@RequestBody Empleado detallesEmpleado){
+        Empleado empleado = repositorio.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException(("No existe el usuario con el ID: "+id)));
+
+        empleado.setNombre(detallesEmpleado.getNombre());
+        empleado.setApellido(detallesEmpleado.getApellido());
+        empleado.setEmail(detallesEmpleado.getEmail());
+
+        Empleado empleadoActualizado = repositorio.save(empleado);
+
+        return ResponseEntity.ok(empleadoActualizado);
+    }*/
 }
